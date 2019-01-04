9acdba79721b8a7bf4353d21f7ac037580f9ae3f
yoshiking <yoshiking0711@gmail.com>
2019-01-04 19:34:39 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,39 @@
+import re
+
+def calc(part):
+    if '*' in part:
+        a = part.split('*')
+        result = int(a[0]) * int(a[1])
+    elif '/' in part:
+        a = part.split('/')
+        result = int(a[0]) / int(a[1])
+    elif '+' in part:
+        a = part.split('+')
+        result = int(a[0]) + int(a[1])
+    elif '-' in part:
+        a = part.split('-')
+        result = int(a[0]) - int(a[1])
+    return result
+
+def matching(sentence):
+    if re.search(r'\d[*/]\d', sentence):
+        match_list = re.findall(r'\d[*/]\d', sentence)
+        calc_result = calc(match_list[0])
+    return calc_result
+
 def main():
-    
-    pass
+    print('Hello. If you give q-key to me, I finish.')
+    print('Available : +, -, *, /, sin(), con(), tan()')
+    while True:
+        sentence = input('>>')
+
+        if 'q' == sentence:
+            print('Goodbye!!!')
+            break
+        
+        result = matching(sentence)
+        
+        print(result)
 
 if __name__ == "__main__":
     main()
