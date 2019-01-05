c99152e54159758fbfc40ec257ecf7f23985e539
yoshiking <yoshiking0711@gmail.com>
2019-01-05 21:23:45 +0900

    complete calc-part

--- calc.py
+++ calc.py
@@ -3,13 +3,13 @@ import math
 
 def math_func(part):
     if 'sin' in part:
-        a = re.match(r'\d+', part)
+        a = re.search(r'\d+', part)
         result = math.sin(int(a.group()))
     elif 'cos' in part:
-        a = re.match(r'\d+', part)
+        a = re.search(r'\d+', part)
         result = math.cos(int(a.group()))
     elif 'tan' in part:
-        a = re.match(r'\d+', part)
+        a = re.search(r'\d+', part)
         result = math.tan(int(a.group()))
     return result
 
@@ -29,13 +29,19 @@ def calc(part):
     return result
 
 def matching(sentence):
-    if re.search(r'sin\(\d+\)|cos\(\d+\)|tan\(\d+\)', sentence):
-        match_list = re.findall(r'sin\(\d+\)|cos\(\d+\)|tan\(\d+\)', sentence)
-        calc_result = math_func(match_list[0])
-    elif re.search(r'\d+[*/]\d+', sentence):
-        match_list = re.findall(r'\d+[*/]\d+', sentence)
-        calc_result = calc(match_list[0])
-    return calc_result
+    while re.search(r'\D', sentence):
+        if re.search(r'sin\(\d+\)|cos\(\d+\)|tan\(\d+\)', sentence):
+            match_list = re.findall(r'sin\(\d+\)|cos\(\d+\)|tan\(\d+\)', sentence)
+            calc_result = math_func(match_list[0])
+        elif re.search(r'\d+[*/]\d+', sentence):
+            match_list = re.findall(r'\d+[*/]\d+', sentence)
+            calc_result = calc(match_list[0])
+            sentence = sentence.replace(match_list[0], str(calc_result))
+        elif re.search(r'\d+[+-]\d+', sentence):
+            match_list = re.findall(r'\d+[+-]\d+', sentence)
+            calc_result = calc(match_list[0])
+            sentence = sentence.replace(match_list[0], str(calc_result))
+    return sentence
 
 def main():
     message = 'Hello. If you give q-key to me, I finish.\nAvailable : +, -, *, /, sin(), con(), tan()\nNote : Don\'t include space!'
